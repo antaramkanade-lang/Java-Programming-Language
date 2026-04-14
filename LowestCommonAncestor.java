5d5390cb9bc3407b04648cd6454128d275e85a35
Antara Kanade <antaramkanade@gmail.com>
2026-04-14 15:48:10 +0530

    Added new java file

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+//T.C=O(n).
+public class LowestCommonAncestor {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // Approach 1:
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+        if (foundLeft || foundRight) {
+            return true;
+        }
+        path.remove(path.size() - 1);// removed node if no ancestor found.
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+        // last common ancestor:
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+        // last equal node=is at i-1th index:
+        Node lca = path1.get(i - 1);
+        return lca;
+    }
+
+    // Approach 2:
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+        // leftLca=value rightLca=null:
+        if (rightLca == null) {
+            return leftLca;
+        }
+        if (leftLca == null) {
+            return rightLca;
+        }
+        return root;
+    }
+
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println(lca(root, 4, 5).data); // it will give 2 as lca.
+        System.out.println(lca2(root, 4, 5).data);
+    }
+}
