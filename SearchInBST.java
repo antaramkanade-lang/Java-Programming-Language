49df232feeb362ba52c16d7d578e6f86dfa62099
Antara Kanade <antaramkanade@gmail.com>
2026-04-16 01:19:56 +0530

    Added new java file

---
+++ SearchInBST.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+//T.C=O(H)=h is height because it traverse from root to leaf.
+public class SearchInBST {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            // right and left are by default null here.
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            // insert in left side:
+            root.left = insert(root.left, val);
+        } else {
+            // insert in right side:
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == key) {
+            return true;
+        }
+        if (root.data > key) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String args[]) {
+        int values[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        if (search(root, 1)) {
+            System.out.println("key found");
+        } else {
+            System.out.println("key not found");
+        }
+    }
+}
